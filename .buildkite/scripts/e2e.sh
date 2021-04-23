887b3703801f0b75e2b6cf336760b8871b5ba0a1
Christopher Maier <chris@graplsecurity.com>
2021-04-23 19:43:33 -0400

    simplify
    Signed-off-by: Christopher Maier <chris@graplsecurity.com>

--- .buildkite/scripts/e2e.sh
+++ .buildkite/scripts/e2e.sh
@@ -2,12 +2,6 @@
 
 set -euo pipefail
 
-capture_logs() {
-    python3 etc/ci_scripts/dump_artifacts.py --compose-project "grapl-e2e_tests"
-}
-
-trap capture_logs EXIT
-
 export GRAPL_LOG_LEVEL="DEBUG"
 export DUMP_ARTIFACTS="True"
 
--- .buildkite/scripts/integration.sh
+++ .buildkite/scripts/integration.sh
@@ -2,12 +2,6 @@
 
 set -euo pipefail
 
-capture_logs() {
-    python3 etc/ci_scripts/dump_artifacts.py --compose-project "grapl-integration_tests"
-}
-
-trap capture_logs EXIT
-
 echo "unset RUSTC_WRAPPER" > rust_env.sh
 chmod 777 rust_env.sh
 
