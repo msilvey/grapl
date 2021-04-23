53fab9662ebc6eaa9d28271adf36fbed40825e1d
Christopher Maier <chris@graplsecurity.com>
2021-04-23 12:25:24 -0400

    blah
    Signed-off-by: Christopher Maier <chris@graplsecurity.com>

--- .buildkite/scripts/investigate_pants.sh
+++ .buildkite/scripts/investigate_pants.sh
@@ -6,5 +6,7 @@ env
 make lint-python
 
 echo "--- failing thing"
+ls -alh .cache
 cat .cache/pex_root/venvs/d8105fd40948579fedc63bb8ddd363d251b7db7a/5dc7bde7966ce85a9f3820e78694dc0e8d63768d/pex
+cat ~/.cache/pex_root/venvs/d8105fd40948579fedc63bb8ddd363d251b7db7a/5dc7bde7966ce85a9f3820e78694dc0e8d63768d/pex
 echo "--- done"
